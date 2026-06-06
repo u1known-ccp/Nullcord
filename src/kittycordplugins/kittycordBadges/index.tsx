8dd4726699a38cd3565c507995f9c58ad499e1e1
Maxi N. <98279618+CenturyRV@users.noreply.github.com>
2026-06-06 05:52:29 +0200

    Add KittycordBadges: a "Kittycord Developer" profile badge
    Registers a profile badge (the Kittycord icon, tooltip "Kittycord Developer") that everyone
    running Kittycord sees on the developers' profiles. Developer Discord IDs are listed in a set so
    more can be added later. Enabled by default so the badge shows widely.

---
+++ src/kittycordplugins/kittycordBadges/index.tsx
@@ -0,0 +1,40 @@
+/*
+ * Kittycord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { addProfileBadge, BadgePosition, ProfileBadge, removeProfileBadge } from "@api/Badges";
+import definePlugin from "@utils/types";
+
+import { BRAND_ICON } from "../../branding";
+
+// Discord user IDs of Kittycord developers. Add more here as the team grows.
+const KITTYCORD_DEVELOPERS = new Set<string>([
+    "432588595845398548" // CenturyRV - creator
+]);
+
+const DeveloperBadge: ProfileBadge = {
+    id: "kittycord-developer",
+    description: "Kittycord Developer",
+    iconSrc: BRAND_ICON,
+    position: BadgePosition.START,
+    link: "https://github.com/CenturyRV/Kittycord",
+    shouldShow: ({ userId }) => KITTYCORD_DEVELOPERS.has(userId)
+};
+
+export default definePlugin({
+    name: "KittycordBadges",
+    description: "Shows a Kittycord Developer badge on the profiles of Kittycord's developers. Visible to everyone running Kittycord.",
+    authors: [{ name: "Kittycord", id: 0n }],
+    dependencies: ["BadgeAPI"],
+    enabledByDefault: true,
+
+    start() {
+        addProfileBadge(DeveloperBadge);
+    },
+
+    stop() {
+        removeProfileBadge(DeveloperBadge);
+    }
+});
